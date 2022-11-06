08db976e77aadf22c08a378977dc0e4f2120c434
Daria <daria.kiseleva@mail.mcgill.ca>
2022-11-06 02:13:24 -0500

    Write useMultistepForm hook

---
+++ src/hooks/useMultistepForm.ts
@@ -0,0 +1,37 @@
+import { ReactElement, useState } from "react";
+
+//Takes ALL steps, which are react elements
+const useMultistepForm = (steps: ReactElement[]) => {
+  const [currentStepIndex, setCurrentStepIndex] = useState(0);
+
+  //ADVANCE THROUGH STEPS FORWARD OR BACK WITHIN BOUNDS
+
+  const next = () => {
+    setCurrentStepIndex((oldIndex) => {
+      if (oldIndex == steps.length - 1) return oldIndex;
+      return oldIndex + 1;
+    });
+  };
+
+  const prev = () => {
+    setCurrentStepIndex((oldIndex) => {
+      if (oldIndex == 0) return oldIndex;
+      return oldIndex - 1;
+    });
+  };
+
+  const goToStep = (index: number) => {
+    setCurrentStepIndex(index);
+  };
+
+  //RETURN THINGS FROM THE HOOK
+  return {
+    steps,
+    currentStepIndex,
+    step: steps[currentStepIndex],
+    next,
+    prev,
+  };
+};
+
+export { useMultistepForm };
